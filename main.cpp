77c791428ddd7ecdbc039df9a2022a9d43c0b872
a.bensalih <abdelghaniben40@gmail.com>
2024-12-28 22:03:16 +0100

    start ed adding the blueprint of tasks class

--- main.cpp
+++ main.cpp
@@ -2,46 +2,68 @@
 #include <string>
 #include <vector>
 using namespace std;
+
 class Task{
-    private:
-          int id;
-          string title;
-          string description;
-          string status; // To Do, In Progress, Done
-    public:
-          Task( int newId, string newTitle, string newDescription, string newStatus ){
-            id = newId;
-            title = newTitle;
-            description = newDescription;
-            status = newStatus;
-          }
+  private:
+  static int idIncrement;
+  string description;
+  string status;
+  string createdAt;
+  string updatedAt;
+  int id;
 
-          
 
-          void display(){
-            cout << "id: " <<id<< "\ntitle: " <<title<< "\ndescription: " <<description<< "\nstatus: " << status;
-          }
+  public:
+  Task(string DESCRIPTION, string STATUS){
+    description = DESCRIPTION;
+    status = STATUS;
+    id = idIncrement++;
+  }
 
 };
 
+int Task::idIncrement=0;
 
+/*
+Each task should have the following properties:
+
+id: A unique identifier for the task
+description: A short description of the task
+status: The status of the task (todo, in-progress, done)
+createdAt: The date and time when the task was created
+updatedAt: The date and time when the task was last updated
+*/
 
 
 
 
 int main(int argc, char *argv[]){
-  string finish;
-
-  while (finish != "finish") {
-    cout << "task-CLI >> ";
-    cin >> finish;
-
-
-  }
-
+  while(true){
+    cout << "1. Add a task\n2. Update a task\n3. Delete a task\n4. Mark a task as in progress\n5. Mark a task as done\n6. List all tasks\n7. List all tasks that are done\n8. List all tasks that are not done\n9. List all tasks that are in progress\n10. Exit\n";
+    int choice;
+    cin >> choice;
+    string description;
+    string status;
   
-    return 0;
-
+    switch (choice)
+    {
+    case 1:
+      cout <<"# adding a new task"<<endl;
+      cout <<"task description: ";
+      cin>> description;
+      cout <<" task status todo,in-progress, done: ";
+      cin>> status;
+      Task task(description, status);
+          
+     
+      
+      break;
+    
+    default:
+      break;
+    }
+    
+  }
 }
 
 /*
